bcad4559b2592d604139ee4f3cc4714d1c21bab2
ShrutiLakhani <82932172+ShrutiLakhani@users.noreply.github.com>
2021-11-22 11:41:18 +0000

    Disabled the 3rd card option

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let cardArr = [card1, card2, card3]
 const firstCard = document.querySelector("#card1");
 const secCard = document.querySelector("#card2");
-const btnStart = document.getElementById("btns");
-var   btnNew = document.getElementById("btns");
+const thirdCard = document.querySelector("#card3");
+const btnStart = document.getElementById("btns-start");
+var   btnNew = document.getElementById("btns-new");
 outputVal = document.querySelector("#sum-el");
 outputVal1 = document.querySelector("#message-el");
 btnStart.addEventListener("click", function() 
@@ -12,41 +14,35 @@ drawCard();
 
 function drawCard()
 {
-
-const sum = Number(card1.value) + Number(card2.value) ;
+const sum = Number(cardArr[0].value) + Number(cardArr[1].value) + Number(cardArr[2].value);
 sumOfCards(sum);
 outputVal.textContent = "Sum:" + sum; 
 }
 
 function sumOfCards(sum)
 {
-  
   if (sum == 21)
   {
-    
-    
-    outputVal1.textContent = "You have hit a Jackpot!!!";
+   outputVal1.textContent = "You have got a BlackJack!!!";
   }
   else if (sum > 21)
   {
-    
-     outputVal1.textContent = "You are out of game!!!";
+   outputVal1.textContent = "You are out of game!!!";
   }
   else
   {
-    
-     outputVal1.textContent = "Do you want to draw a new card?!!!";
+   outputVal1.textContent = "Do you want to draw a new card?!!!";
   }
 }
-
-function Newcard()
+btnNew.addEventListener("click", function() 
 {
- 
+console.log("comes here");
+thirdCard.disabled = false;
+newCard();  
+})
+
+function newCard()
+{
+ drawcard();
 }
-
-
-
-
-//const sum = Number(firstCard.value) + Number(secCard.value);
-//console.log(sum.value);
 
