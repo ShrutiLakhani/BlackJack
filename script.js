4b19dad63bd94f747b71498fbc151b83519d857b
ShrutiLakhani <82932172+ShrutiLakhani@users.noreply.github.com>
2021-11-23 12:27:58 +0000

    Push the Third Card

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
-let cardArr = [card1, card2, card3]
+let cardArr = [card1, card2]
 const firstCard = document.querySelector("#card1");
 const secCard = document.querySelector("#card2");
 const thirdCard = document.querySelector("#card3");
 const btnStart = document.getElementById("btns-start");
+//var thirdCard = document.getElementById("card3").value;
+
+
 var   btnNew = document.getElementById("btns-new");
+var sum = 0;
 outputVal = document.querySelector("#sum-el");
 outputVal1 = document.querySelector("#message-el");
 btnStart.addEventListener("click", function() 
 {
-drawCard();
-  
+drawCard();  
 })
 
 function drawCard()
 {
-const sum = Number(cardArr[0].value) + Number(cardArr[1].value) + Number(cardArr[2].value);
+    //console.log("comes here");
+    //console.log(firstCard.value);
+    //console.log(secCard.value);
+    //console.log(thirdCard.value);
+    //thirdCard.value = NaN
+    //thirdCard = thirdCard || 0; 
+   //console.log(secCard.value);
+    //console.log(thirdCard.value);
+  //console.log(cardArr[0].value);
+  //console.log(cardArr[1].value);
+  //console.log(cardArr[2].value);
+  //console.log(thirdCard.value);
+const sum = +(Number(card1.value) + Number(card2.value)+ Number(card3.value));
+
 sumOfCards(sum);
+console.log(sum);
 outputVal.textContent = "Sum:" + sum; 
 }
 
@@ -38,11 +55,13 @@ btnNew.addEventListener("click", function()
 {
 console.log("comes here");
 thirdCard.disabled = false;
+console.log("comes here-1");
 newCard();  
 })
-
 function newCard()
 {
+ //let thirdCard = 0; 
+ cardArr.push(thirdCard);
  drawcard();
 }
 
